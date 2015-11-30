4aa052b581b425eaaa52951186c7a6c35f93f8f9
Glen Cooney <glen@glencooney.com>
2015-11-30 18:07:25 -0500

    Start of To Do List Items

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,21 +6,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
 
-  # before_save { self.email = email.downcase }
-  #
-  # EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  #
-  # validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-  #
-  # validates :password, presence: true, length: { minimum: 6 }, if: 'password_digest.nil?'
-  # validates :password, length: { minimum: 6 }, allow_blank: true
-  #
-  # validates :email,
-  #           presence: true,
-  #           uniqueness: { case_sensitive: false },
-  #           length: { minimum: 3, maximum: 100 },
-  #           format: { with: EMAIL_REGEX }
+  has_many :items
 
-  # NOTE Causes errors with Devise?
-  # has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'items/create'
+
   # get 'user/show'
 
   # get 'users_controller/show'
 
-  devise_for :users
+  devise_for :users do
+    resources :items, only: [:create]
+  end
 
   # root to: "home#index"
   root to: 'users#show'
