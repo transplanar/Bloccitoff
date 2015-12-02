9475d3c32dc152ce4c35ff43a6f65344b0a1fb79
Glen Cooney <glen@glencooney.com>
2015-12-02 17:37:06 -0500

    Code cleanup and remove route nesting

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,24 +5,12 @@ class ItemsController < ApplicationController
     if @item.save
       flash[:notice] = "Task saved"
       # TODO Remove this line after Ajax implementation
-      # FIXME nesting under user isn't working
-      # redirect_to [current_user, @item]
       redirect_to current_user
-      # user_path(current_user)
     else
       flash[:error] = "Error saving task. Please try again."
       # TODO Remove this line after Ajax implementation
       render :new
     end
-
-    # if @topic.save
-    #       @topic.labels = Label.update_labels(params[:topic][:labels])
-    #       redirect_to @topic, notice: 'Topic was saved successfully.'
-    #     else
-    #       flash[:error] = 'Error creating topic. Please try again.'
-    #       render :new
-    #     end
-
 
     #  redirect_to current_user
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,8 @@
 Rails.application.routes.draw do
-  # get 'items/create'
-
-  # get 'user/show'
-
-  # get 'users_controller/show'
-
-  #REVIEW is routing for items correct?
   devise_for :users
 
-# REVIEW Needed?
-  resources :users do
-    resources :items, only: [:create]
-  end
-  # devise_for :users do
-    # resources :items, only: [:create]
-  # end
+  resources :users, only: [:show]
+  resources :items, only: [:create]
 
-# NOTE Test of nesting
-  # resources :items, only: [:create]
-
-  # root to: "home#index"
   root to: 'users#show'
 end
