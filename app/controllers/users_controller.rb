2cffd830559bbbb899adad2fccf1f05c91b4d640
Glen Cooney <glen@glencooney.com>
2015-12-08 19:06:29 -0500

    Show all users PROGRESS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,19 @@ class UsersController < ApplicationController
 
   def show
     # @user = User.find(params[:id])
-    @user = current_user
-    @items = Item.all
+    # @user = current_user
+    @user = User.find(params[:id])
+    # @user = current_user
+    # @items = Item.all
+    @items = @user.items.all
+  end
+
+  def index
+    #TODO Adjust visibility of other user profiles
+    @users = User.all
+  end
+
+  def current_user_home
+    redirect_to current_user
   end
 end
