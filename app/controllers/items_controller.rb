77cb7f3e2c33172d183b2d0efbad36bf858be614
Glen Cooney <glen@glencooney.com>
2015-12-08 19:31:19 -0500

    Attempting to get Ajax working on other user profiles

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,21 +14,30 @@ class ItemsController < ApplicationController
 
     # NOTE New
 
-    @item = current_user.items.new(item_params)
+    # @item = current_user.items.new(item_params)
+    # @item = @user.items.new(item_params)
+    @item = Item.new(item_params)
+    # TODO Make sure user cannot delete items they do not own
+    # @item = Item.new(item_params)
     @item.save
 
     respond_to do |format|
-      format.html { redirect_to current_user }
+      # format.html { redirect_to current_user }
+      format.html { redirect_to @user }
       format.js
     end
   end
 
   def destroy
-    @item = current_user.items.find(params[:id])
+    # @item = current_user.items.find(params[:id])
+    # @item = @user.items.find(params[:id])
+    @item = Item.find(params[:id])
+    # @item = Item.find(params[:id])
     @item.destroy
 
     respond_to do |format|
-      format.html { redirect_to current_user }
+      # format.html { redirect_to current_user }
+      format.html { redirect_to @user }
       format.js
     end
   end
